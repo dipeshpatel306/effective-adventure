0a059b9c6467038ce1a7d779685150459a535056
Dan Felicetta <danf@entegration.net>
2015-02-26 23:32:50 -0500

    add new schema file

---
+++ bsn/app/Config/Schema/schema_bsn_v1.sql
@@ -0,0 +1,8 @@
+alter table risk_assessments
+add column `question_49` varchar(5) after `question_48`, 
+add column `question_50` varchar(5) after `question_49`, 
+add column `question_51` varchar(5) after `question_50`, 
+add column `question_52` varchar(5) after `question_51`, 
+add column `question_53` varchar(5) after `question_52`, 
+add column `question_54` varchar(5) after `question_53`, 
+add column `question_55` varchar(5) after `question_54`;
